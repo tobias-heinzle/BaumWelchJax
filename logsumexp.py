b6eb7c9f65566c3e2f05ed8c596ee8d74684dd5d
Tobias Heinzle <tobias.heinzle@hotmail.de>
2025-10-03 12:17:55 +0200

    ADD logsumexp explanation since it is used in the loglikelihood compilation

--- logsumexp.py
+++ logsumexp.py
@@ -3,15 +3,10 @@ import jax
 
 
 def logsumexp(x):
+    """
+    Performs the logsumexp trick for normalizing logprobabilities.
+    See https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/
+    """
     c = jnp.max(x)
-
-    # jax.debug.print("x: {}", x, ordered=True)
-    # jax.debug.print("c: {}", c, ordered=True)
-    # jax.debug.print("x -c: {}", x - c, ordered=True)
-    # jax.debug.print("exp(x-c): {}", jnp.exp(x - c), ordered=True)
-    # jax.debug.print("sum(exp(x-c)): {}", jnp.sum(jnp.exp(x - c)), ordered=True)
-    # jax.debug.print("log(sum(exp(x-c))): {}",
-    #                 jnp.log(jnp.sum(jnp.exp(x - c))), ordered=True)
-
     diff = jnp.nan_to_num(x - c, -jnp.inf)
     return c + jnp.log(jnp.sum(jnp.exp(diff)))
