67398b3b3b5c1a29adfb61449b3e9872fac36eb5
Tobias Heinzle <tobias.heinzle@hotmail.de>
2025-11-14 09:34:05 +0100

    FIX inference bug TEST on some real data

--- inference.py
+++ inference.py
@@ -231,7 +231,7 @@ def baum_welch_log(
         # M - step
         # Average over all time steps and normalize along rows => new estimate for T
         T = logsumexp(xi, axis=0)
-        T -= logsumexp(T, axis=1)
+        T -= logsumexp(T, axis=-1)[..., None]
 
         O = lax.map(lambda o: logsumexp(
             jnp.log(obs.ravel() == o)[:, None] + gamma, axis=0), jnp.arange(m)).T
