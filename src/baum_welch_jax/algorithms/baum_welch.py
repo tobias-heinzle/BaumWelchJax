ADD some guardrails and exceptions for parameter freezing functionality ADD tests for parameter freezing FIX shared_mu normalization

src/baum_welch_jax/algorithms/baum_welch.py
@@ -8,13 +8,15 @@ from jax.scipy.special import logsumexp
 from jax import Array
 
 from ..util import wrapped_jit, normalize_rows, standardize_shapes
-from ..models import HiddenMarkovParameters, IterationState, FreezeConfig, FreezeMasks
+from ..models import HiddenMarkovParameters, IterationState, FreezeConfig, FreezeMasks, assert_valid_hmm
 from .forward_backward import forward_backward
 from .likelihoods import log_likelihood
 from .._precision import _warn_if_fp32
 
 
 # TODO: How to handle ragged seqeunces of different length?
+#       In principle, also final state distributions could be provided and estimated during the forward backward pass.
+#       Look into this in more detail!
 
 def _maximization_step(obs: Array, gamma: Array, xi: Array, m: int) -> tuple[Array, Array]:
 
@@ -92,6 +94,12 @@ def baum_welch(obs: Array,
     '''
     
     _warn_if_fp32()
+
+    try:
+        assert_valid_hmm(initial_params)
+    except ValueError as exc:
+        raise ValueError('`initial_params` is not a valid set of parameters!') from exc
+
     
     if mode == 'log':
         if not initial_params.is_log:
@@ -127,13 +135,29 @@ def baum_welch(obs: Array,
         return jnp.any(jnp.array([state.terminated, is_converged]))
 
     # Set up freeze configuration
-    # TODO: This freeze mask setup is not correct yet!
-    # The algorithm must be adapted to correctly handle frozen paramters. 
+    # This freeze mask setup is not finished yet!
+    # Works for fixing an entire matrix or row, but does not perform correctly for
+    # fixing of single indices!
+    def is_correct(masks: FreezeMasks) -> bool:
+        # Check if the freeze mask has the supported structure
+        checked_mask = jax.tree.map(lambda leaf: 
+            jnp.all(
+                jnp.all(leaf == True, axis=-1) 
+                | jnp.all(leaf == False, axis=-1)
+            ),
+            masks)
+        
+        return checked_mask.T and checked_mask.O and checked_mask.mu
+    
+    # TODO: Adapt the algorithm must to correctly handle single frozen paramters. 
     # In particular, the re-normalization of the new parameters must be performed considering only
     # the remaining probability mass that is not yet used by the frozen parameters.
     if isinstance(freeze_config, FreezeConfig):
         freeze_masks = freeze_config.create_masks(initial_params)
     else:
+        if not is_correct(freeze_config):
+            raise ValueError('Invalid `freeze_config`: Only entire rows can be frozen, not single entries!')
+
         freeze_masks = freeze_config
 
     
@@ -151,7 +175,10 @@ def baum_welch(obs: Array,
     if shared_mu:
         final_state = final_state.replace_mu(jnp.mean(final_state.params.mu, axis=0))
 
-    return final_state.squeeze()
+    final_state = final_state.squeeze()
+    assert_valid_hmm(final_state.params)
+
+    return final_state
 
 @wrapped_jit(static_argnames=["stop_criterion", "max_iter", "mode", "shared_mu"])
 def _baum_welch_impl(obs: Array,
@@ -165,6 +192,7 @@ def _baum_welch_impl(obs: Array,
     the same lenght.'''
 
     m_obs = initial_params.O.shape[-1]
+    n_obs = obs.shape[0]
     n_mu = initial_params.mu.shape[0]
     is_log = (mode == 'log')
 
@@ -193,7 +221,12 @@ def _baum_welch_impl(obs: Array,
 
                 # TODO: How should this estimate be weighted? 
                 # Is it really correct to just take the mean here?
-                mu = jnp.mean(gamma[:, 0], axis=0)[None, ...].repeat(n_mu, axis=0)
+                
+                if is_log:
+                    mu = logsumexp(gamma[:, 0], axis=0, keepdims=True).repeat(n_mu, axis=0) - jnp.log(n_obs)
+                else:
+                    mu = jnp.mean(gamma[:, 0], axis=0, keepdims=True).repeat(n_mu, axis=0)
+
             else:
                 # Separate mu estimates for each sequence
                 mu = gamma[:, 0]

tests/test_baum_welch.py
@@ -8,7 +8,7 @@ import pytest
 
 from baum_welch_jax import PrecisionWarning
 from baum_welch_jax.algorithms import baum_welch, generate_sequence
-from baum_welch_jax.models import HiddenMarkovParameters, assert_valid_hmm, FreezeConfig
+from baum_welch_jax.models import HiddenMarkovParameters, assert_valid_hmm, FreezeConfig, FreezeMasks
 from baum_welch_jax.util import normalize_rows
 
 from conftest import *
@@ -400,8 +400,8 @@ def test_likelihood_lower_bound_increase(mode):
     _O = uniform(key_O, (n,m)) * O_structure
     _mu = uniform(key_mu, n)
 
-    _T = _T / jnp.sum(_T, axis=1)[:, None]
-    _O = _O / jnp.sum(_O, axis=1)[:, None]
+    _T = _T / jnp.sum(_T, axis=1, keepdims=True)
+    _O = _O / jnp.sum(_O, axis=1, keepdims=True)
     _mu = _mu / jnp.sum(_mu)
     init_guess = HiddenMarkovParameters(_T, _O, _mu)
 
@@ -422,7 +422,6 @@ def test_likelihood_lower_bound_increase(mode):
     assert jnp.all(averaged_increases >= MONOTONICITY_TOLERANCE), ',\n '.join(map(str, averaged_increases.tolist()))
 
 
-@pytest.mark.debug
 @pytest.mark.parametrize('mode', ['regular', 'log'])
 @enable_x64
 def test_multiple_sequence_8_states(mode):
@@ -439,7 +438,6 @@ def test_multiple_sequence_8_states(mode):
     assert not result.terminated
 
 # Make sure that freezing of parameters works as intendedG
-@pytest.mark.debug
 @pytest.mark.parametrize('mode', ['regular', 'log'])
 @pytest.mark.parametrize('freeze_config', [
     FreezeConfig(T=True),
@@ -453,7 +451,7 @@ def test_freeze_config(mode, freeze_config):
         normalize_rows(jax.random.uniform(key(0), (n, n))), 
         normalize_rows(jax.random.uniform(key(1), (n, m))), 
         normalize_rows(jax.random.uniform(key(2), (n, ))),
-        is_log = (mode=='log')).astype(jnp.float64)
+    ).astype(jnp.float64)
     
     _result = baum_welch(
         TEST_SEQUENCES_5_STEPS[0], 
@@ -463,6 +461,8 @@ def test_freeze_config(mode, freeze_config):
         freeze_config=freeze_config)
 
     result = _result.params
+    if mode == 'log':
+        result = result.to_prob()
     if freeze_config.T:
         assert jnp.allclose(result.T, hmm.T)
     else:
@@ -475,3 +475,86 @@ def test_freeze_config(mode, freeze_config):
         assert jnp.allclose(result.mu, hmm.mu)
     else:
         assert not jnp.allclose(result.mu, hmm.mu)
+
+
+@pytest.mark.parametrize('mode', ['regular', 'log'])
+@enable_x64
+def test_freeze_config_rows(mode):
+    hmm = HiddenMarkovParameters(
+        normalize_rows(jax.random.uniform(key(0), (2, 2))), 
+        normalize_rows(jax.random.uniform(key(1), (2, 2))), 
+        normalize_rows(jax.random.uniform(key(2), (2, ))))
+    freeze_masks = FreezeMasks(
+        T = jnp.array([[True, True], [False, False]]),
+        O = jnp.array([[False, False], [True, True]]),
+        mu = jnp.array([True, True])
+    )
+
+
+    _result = baum_welch(
+        jnp.array([1,1,1,1,0,0,0,1,1,1,1]).astype(jnp.int32), 
+        hmm, 
+        max_iter=5, 
+        mode=mode, 
+        freeze_config=freeze_masks)
+
+    result = _result.params
+    if mode == 'log':
+        result = result.to_prob()
+    
+    assert_valid_hmm(result)
+    assert jnp.allclose(result.T[0], hmm.T[0])
+    assert not jnp.allclose(result.T[1], hmm.T[1])
+    assert jnp.allclose(result.O[1], hmm.O[1])
+    assert not jnp.allclose(result.O[0], hmm.O[0])
+    assert jnp.allclose(result.mu, hmm.mu)
+
+@pytest.mark.parametrize('mode', ['regular', 'log'])
+@pytest.mark.parametrize('param', ['T', 'O', 'mu'])
+@enable_x64
+def test_freeze_config_error(mode, param):
+    hmm = HiddenMarkovParameters(
+        normalize_rows(jax.random.uniform(key(0), (2, 2))), 
+        normalize_rows(jax.random.uniform(key(1), (2, 2))), 
+        normalize_rows(jax.random.uniform(key(2), (2, ))))
+    
+    T = jnp.array([[True, True], [False, False]])
+    O = jnp.array([[True, True], [False, False]])
+    mu = jnp.array([True, True])
+    
+    # Test if single frozen parameters are caught and an 
+    # Exception is thrown
+    match param:
+        case 'T':
+            T = T.at[0,0].set(False)
+        case 'O':
+            O = O.at[0,0].set(False)
+        case 'mu':
+            mu = mu.at[0].set(False)
+
+    freeze_masks = FreezeMasks(T, O, mu)
+
+    with pytest.raises(ValueError):
+        baum_welch(
+        jnp.array([1,1,1,1,0,0,0,1,1,1,1]).astype(jnp.int32), 
+        hmm, 
+        max_iter=5, 
+        mode=mode, 
+        freeze_config=freeze_masks)
+
+    
+
+@pytest.mark.parametrize('mode', ['regular', 'log'])
+@enable_x64
+def test_input_validation(mode):
+    hmm = HiddenMarkovParameters(
+        jax.random.uniform(key(0), (2, 2)), 
+        normalize_rows(jax.random.uniform(key(1), (2, 2))), 
+        normalize_rows(jax.random.uniform(key(2), (2, ))))
+    
+    with pytest.raises(ValueError):
+        baum_welch(
+            jnp.array([0,1]).astype(jnp.int32), 
+            hmm, 
+            max_iter=5, 
+            mode=mode, )